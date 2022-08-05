3d3f8f98affcca4bd856783c34ef25ff8e8d1a21
Zachary Sain <zachsain@gmail.com>
2022-08-05 15:39:24 -0500

    commit tree a196d6feeaa11e28565f512fe7fa4e5f63fdf4b2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,42 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    # ,  uniqueness: { case_sensitive: true }
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, :inclusion => {:in => ["Fiction", "Non-Fiction"]}
+    # validate :title_is_clickbait
+    validate :true_facts_method
+
+    def true_facts_method 
+
+        titles_allowed = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+        # validate :title_is_clickbait
+        # def title_is_clickbait
+        #     if ["Won't Believe", "Secret", "Top [number]", "Guess"].none? {|string| title && title.include?(string)}
+        #         errors.add(:title, "needs more juice")
+        #     end
+        # end
+        
+        # titles_allowed.each do |str| 
+        #    if !title&.include? str
+        #     errors.add(:title, "not acceptable")
+        #    end 
+        # end 
+
+       v = titles_allowed.none? { |string| title && title.include?(string) }
+
+       if v 
+         erros.add(:title, "not acceptable")
+       end 
+    end 
+
+
+    def title_is_clickbait
+        if ["Won't Believe", "Secret", "Top [number]", "Guess"].none? {|string| title && title.include?(string)}
+            errors.add(:title, "needs more juice")
+        end
+    end
+
 end
