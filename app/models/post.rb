7826ec84ac4c832933c874e515a7b1250e77cd61
Zachary Sain <zachsain@gmail.com>
2022-08-06 13:08:35 -0500

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    # ,  uniqueness: { case_sensitive: true }
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, :inclusion => {:in => ["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbait
+   
+
+    def title_is_clickbait
+        if ["Won't Believe", "Secret", "Top [number]", "Guess"].none? {|string| title && title.include?(string)}
+            errors.add(:title, "needs more juice")
+        end
+    end
+
 end
